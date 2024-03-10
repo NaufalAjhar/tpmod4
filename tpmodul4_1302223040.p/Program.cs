8fa71696e8c1bc3695aed821458e7a75391191be
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-03-10 20:29:11 +0700

    inisialisasi projek

---
+++ tpmodul4_1302223040.p/Program.cs
@@ -0,0 +1,12 @@
+﻿using static tpmodul4_1302223040.p.KodePos;
+
+namespace tpmodul4_1302223040.p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
